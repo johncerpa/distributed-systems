8a6cca9121e9fe2e1c5b43d03d734910fe658c89
Jhon Cerpa <johncerpa98@gmail.com>
2021-04-04 12:44:35 -0500

    Leader election implementation

--- distributed.systems/src/main/java/LeaderElection.java
+++ distributed.systems/src/main/java/LeaderElection.java
@@ -1,8 +1,8 @@
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class LeaderElection implements Watcher {
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
@@ -11,6 +11,9 @@ public class LeaderElection implements Watcher {
         respond in `SESSION_TIMEOUT` milliseconds it will consider the client as disconnected
     */
     private static final int SESSION_TIMEOUT = 3000;
+    private static final String ELECTION_NAMESPACE = "/election";
+    private String currentZnodeName;
+
     private ZooKeeper zooKeeper;
 
     public void connectToZookeeper() throws IOException {
@@ -48,9 +51,35 @@ public class LeaderElection implements Watcher {
         }
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    /*
+        Creates an ephemeral znode with no data in it, with no access control list
+        This type of node gets deleted when it gets disconnected from Zookeeper
+    */
+    public void volunteerForLeadership() throws KeeperException, InterruptedException {
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        System.out.println("Znode name: " + znodeFullPath);
+        this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+    }
+
+    public void electLeader() throws KeeperException, InterruptedException {
+        List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+
+        Collections.sort(children);
+        String smallestChild = children.get(0);
+
+        if (smallestChild.equals(currentZnodeName)) {
+            System.out.println("I am the leader");
+        } else {
+            System.out.println("I am not the leader. The current leader is " + smallestChild);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         LeaderElection leaderElection = new LeaderElection();
         leaderElection.connectToZookeeper();
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
         leaderElection.run();
         leaderElection.close();
         System.out.println("Disconnected from Zookeeper, exiting application.");
